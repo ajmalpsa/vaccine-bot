7e7623dfb37e5404c5ee123d3deb7d9231fb6d65
ajmalpsa <47412801+ajmalpsa@users.noreply.github.com>
2021-06-09 16:06:57 +0530

    Updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/",(req, res)=>{
     res.send("bot is working...");
 })
 
-cron.schedule('0 0 * * * *',sendMessageAction);
+cron.schedule('0 15 * * *',sendMessageAction);
 
 
 app.listen(port,'0.0.0.0', ()=>{
